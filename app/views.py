a8420120f10c7227e1023292766ba40869f53741
João Soares <jvsoares082004@gmail.com>
2024-05-17 14:48:35 -0300

    created requests GET and POST

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from app.models import Todo
 
-# Create your views here.
+from app.serializers import TodoSerializer
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST'])
+def todo_list(request):
+    if request.method =='GET':
+        todo = Todo.objects.all()
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
